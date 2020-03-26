403be0c647ec2f3e3fc0c420e0d3d2740c2bea0b
iklevente <k.levi2@outlook.hu>
2020-03-26 22:26:49 +0100

    Some Description

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Fake-LoginPanel-Flooder
- A program to flood a fake login panel with random passwords.
+ A program to flood a fake login panel with random passwords. I made this program to spam a Steam account stealer site called casenorm.fun. It was like hellcase.com, but they stole my friend's steam account. It worked. Spammed them over 1000 tries and the site is still
+ not online.
--- main.py
+++ main.py
@@ -9,27 +9,27 @@ from time import sleep
 
 tries = 1
 
-while (tries < 10000):
+while (tries < 10000): # Set 10000 to how many times you want to spam the stealer site with fake passwords.
 	
 	print tries
 
-	usernameStr = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
-	passwordStr = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+	usernameStr = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8)) # Generate random uppercase username
+	passwordStr = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8)) # Generate random uppercase password
 
 	print "Current generated password is: " + usernameStr
 	print "Current generated password is: " + passwordStr
 
-	browser = webdriver.Chrome("/bin/chromedriver")
-	browser.get(('https://casenorm.fun/'))
-	browser.get(('https://casenorm.fun/auth'))
+	# Chose your chromedriver path. I was using Linux and I downloaded it from https://chromedriver.chromium.org/downloads. Download the one wich matches your chrome/chromium version. You can check your chromium version on html5test.com
+	browser = webdriver.Chrome("/bin/chromedriver") 
+	browser.get(('https://casenorm.fun/auth')) # Login panel's URL address
 
-	username = browser.find_element_by_id('steamAccountName')
-	username.send_keys(usernameStr)
+	username = browser.find_element_by_id('steamAccountName') # Username textbox on login site. (Right click in username textbox --> Inspect element --> search for id="something")
+	username.send_keys(usernameStr) 
 
-	username = browser.find_element_by_id('steamPassword')
+	username = browser.find_element_by_id('steamPassword') # Password textbox on login site. (Right click in password textbox --> Inspect element --> search for id="something")
 	username.send_keys(passwordStr)
 
-	loginButton = browser.find_element_by_id('SteamLogin')
+	loginButton = browser.find_element_by_id('SteamLogin') # Login button on login site. (Right click on login button --> Inspect element --> search for id="something")
 	loginButton.click()
 	sleep(2)
 	browser.close()
